5f8ab3418be7f8559852db95fcce7c3d351de3c4
voja <voja@gmail.com>
2024-05-26 10:53:01 +0200

    06 AddCustomer component

--- src/AddCustomer.jsx
+++ src/AddCustomer.jsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+
 function AddCustomer() {
-    return <div>AddComponent</div>;
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subscribed, setSubscribed] = useState(false);
+
+    return (
+        <div className="col">
+            <h1>Add Customer</h1>
+
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="id" className="form-label">
+                        ID
+                    </label>
+                    <input
+                        className="form-control"
+                        id="id"
+                        value={id}
+                        onChange={(e) => setId(parseInt(e.target.value))}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">
+                        Name
+                    </label>
+                    <input
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                        Email
+                    </label>
+                    <input
+                        className="form-control"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Subscribed</label>
+                    <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="subscribed"
+                        checked={subscribed}
+                        onChange={(e) => setSubscribed(e.target.checked)}
+                    />
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default AddCustomer;
